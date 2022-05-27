8665819c0f158bfa988d9c194f66c4249942422b
KotenkoNata <kotenkonata8@gmail.com>
2022-05-27 16:16:19 +0300

    refactor: rewrite bindTaskEvents function

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("new-task-label");//Add a new task.
+var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
 var incompleteTaskHolder=document.getElementById("to-do-list list");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completed-tasks-list");//completed-tasks
@@ -67,6 +67,7 @@ var addTask=function(){
     console.log("Add Task...");
     //Create a new list item with the text from the #new-task:
     if (!taskInput.value) return;
+
     var listItem=createNewTaskElement(taskInput.value);
 
     //Append listItem to incompleteTaskHolder
@@ -74,6 +75,8 @@ var addTask=function(){
     bindTaskEvents(listItem, taskCompleted);
 
     taskInput.value="";
+
+    console.log(taskInput)
 
 }
 
@@ -156,12 +159,14 @@ addButton.addEventListener("click",addTask);
 addButton.addEventListener("click",ajaxRequest);
 
 
-var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
+var bindTaskEvents=function(taskListItem, checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
     var checkBox=taskListItem.querySelector("input[type=checkbox]");
-    var editButton=taskListItem.querySelector("button.edit");
-    var deleteButton=taskListItem.querySelector("button.delete");
+    var editButton=taskListItem.querySelector("button.to-do-list__button_edit");
+    var deleteButton=taskListItem.querySelector("button.delete-button");
+
+    console.log(checkBox, editButton, deleteButton)
 
 
     //Bind editTask to edit button.
